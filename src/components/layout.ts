8fe9fecdf41dc3d7c6a1d0b17df18b6cb9cab50d
KinjalRadadiya <kinjal01radadiya@gmail.com>
2025-04-15 06:41:41 +0530

    Initial commit

---
+++ src/components/layout.ts
@@ -0,0 +1,6 @@
+export const Layout = () => `
+  <div class="main-layout">
+    <h2>Main LAyout</h2>
+    <p>I’m a Front-End Developer with a passion for clean UI and user experience.</p>
+  </div>
+`;
