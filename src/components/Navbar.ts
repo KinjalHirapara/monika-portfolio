0b0fa9341f891e348b3dcb7378263fceb4898d56
KinjalRadadiya <kinjal01radadiya@gmail.com>
2025-04-22 06:09:00 +0530

    working on header snd about section.

---
+++ src/components/Navbar.ts
@@ -0,0 +1,31 @@
+export function setupNavbarBehavior(): void {
+  const navbar = document.getElementById("navbar-links");
+
+  window.addEventListener("scroll", () => {
+    // Change navbar background on scroll
+    if (window.scrollY > 50) {
+      navbar?.classList.add("bg-black/50", "shadow-md", "backdrop-blur-sm");
+      navbar?.classList.remove("bg-transparent");
+    } else {
+      navbar?.classList.remove("bg-black/50", "shadow-md", "backdrop-blur-sm");
+      navbar?.classList.add("bg-transparent");
+    }
+
+    // // Active link highlight
+    // let current = "";
+    // sections.forEach((section) => {
+    //   const top = section.offsetTop - 100;
+    //   const height = section.offsetHeight;
+    //   if (window.scrollY >= top && window.scrollY < top + height) {
+    //     current = section.id;
+    //   }
+    // });
+
+    // navLinks.forEach((link) => {
+    //   link.classList.remove("text-blue-400", "font-semibold");
+    //   if (link.getAttribute("href") === `#${current}`) {
+    //     link.classList.add("text-blue-400", "font-semibold");
+    //   }
+    // });
+  });
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import "./assets/style.css";
+import { setupNavbarBehavior } from "./components/Navbar";
+
+setupNavbarBehavior();
