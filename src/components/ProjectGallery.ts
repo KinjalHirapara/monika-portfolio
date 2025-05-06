7a2900686752402643363fdfe34c9a2ac4b00f96
KinjalRadadiya <kinjal01radadiya@gmail.com>
2025-05-06 12:43:02 +0530

    complete project section.

--- src/components/ProjectGallery.ts
+++ src/components/ProjectGallery.ts
@@ -2,7 +2,7 @@ import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 export function ProjectGallery() {
-  Fancybox.bind('[data-fancybox="gallery"]', {
+  Fancybox.bind('[data-fancybox^="gallery"]', {
     //
   });
 }
